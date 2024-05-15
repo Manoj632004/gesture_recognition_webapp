9546fea46c144d9f6a40bb573cb48b5f73d73c33
Manoj S <93467109+Manoj632004@users.noreply.github.com>
2024-05-15 19:30:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response
+from new import VideoCapture
+import os
+import signal
+
+app = Flask(__name__)
+video_capture = VideoCapture()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/main_video_feed')
+def main_video_feed():
+    return Response(VideoCapture.gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+"""
+@app.route('/toggle_video', methods=['POST'])
+def toggle_video():
+    video_capture.toggle_video()
+    return 'Video toggled successfully.', 200
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
+
